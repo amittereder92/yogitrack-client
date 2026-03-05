f9b52c779345f10e2e067ed66e5a7ff221cc1d9e
Alex <amittereder92@gmail.com>
2026-03-04 23:34:21 -0500

    add root route

--- src/yogiserver.cjs
+++ src/yogiserver.cjs
@@ -2,12 +2,14 @@ const express = require("express");
 const app = express();
 
 // Serve static files from the public dir
-app.use(express.static("public"));
+app.use(express.static(__dirname + "/public"));
 app.use(express.json());
 
-app.use("/api/instructor", require("../src/routes/instructorRoutes.cjs"));
+app.use("/api/instructor", require("./routes/instructorRoutes.cjs"));
 
-
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/public/index.html");
+});
 
 // Start the web server
 const PORT = process.env.PORT || 8080;
